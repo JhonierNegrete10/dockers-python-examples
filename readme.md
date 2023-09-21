98fec233254a262d0ceb3047a4b0a904ad16dad4
JhonierNegrete10 <jhoniernegretem@gmail.com>
2023-09-21 12:44:04 -0500

    working multi-multiprocessing

---
+++ readme.md
@@ -0,0 +1,2 @@
+docker build . -t python:0.0.2
+docker run -p 8000:8000 -t python:0.0.2 
--- tmp.py
+++ tmp.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI, APIRouter, BackgroundTasks
 from fastapi.responses import JSONResponse
 from fastapi import __version__ as version
+import multiprocessing
+import time
+
 router = APIRouter()
 
 
-def test_func(s: str) -> bool:
+def worker_function(s: str):
+    while True:
+        print(f"~~~ test_func: {s}")
+        time.sleep(2)
 
-    print(f"~~~ test_func: {s}")
-    return True
+
+def test_func(s: str):
+    process = multiprocessing.Process(target=worker_function, args=(s,))
+    # Para que el proceso se detenga cuando la aplicación principal se detenga
+    process.daemon = True
+    process.start()
 
 
-@router.get(
-    "/test",
-)
+@router.get("/test")
 def get_test(background_tasks: BackgroundTasks) -> dict:
-    # Only this task's output will be logged
     background_tasks.add_task(test_func, "one")
     background_tasks.add_task(test_func, "two")
     background_tasks.add_task(test_func, "three")
